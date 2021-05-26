0b324d57987378cadc5eab1084a0392e8ef3a98d
RansAlexander <r0808003@student.thomasmore.be>
2021-05-26 12:06:24 +0200

    Communication back to pi & threading

--- testraspbee.py
+++ testraspbee.py
@@ -1,23 +1,26 @@
+from flask import Flask, render_template, url_for, request, redirect, make_response
+from random import random
+import threading
+import requests
 import RPi.GPIO as GPIO
-import cgitb;
+import cgitb
 import spidev
 import time
 import json
 import random
 cgitb.enable()
-import requests
-from random import random
-from flask import Flask,render_template,url_for,request,redirect, make_response
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(2, GPIO.OUT)
 
 app = Flask(__name__)
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000
+
 
 def readpot(potmeter):
     if ((potmeter > 7) or (potmeter < 0)):
@@ -27,15 +30,14 @@ def readpot(potmeter):
     potout = ((r[1] & 3) << 8) + r[2]
     return potout
 
+
 def blink(pin):
-	#setup GPIO output channel
-	GPIO.setup(pin, GPIO.OUT)
-	GPIO.output(pin, 1)
-	time.sleep(0.5)
-	GPIO.output(pin, 0)
-	time.sleep(0.5)
-
-
+    # setup GPIO output channel
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 1)
+    time.sleep(0.5)
+    GPIO.output(pin, 0)
+    time.sleep(0.5)
 
 
 print("Aan het berekenen...")
@@ -43,56 +45,72 @@ print("Aan het berekenen...")
 warmte = readpot(0)
 oogst = readpot(1)
 
+
 @app.route('/', methods=["GET", "POST"])
 def main():
+    status = request.args.get('status')
+    if status == "on":
+        GPIO.output(2, GPIO.HIGH)
+    elif status == "off":
+        GPIO.output(2, GPIO.LOW)
     return render_template('index.html')
+
+
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    Temperature = readpot(0)
-    data = [time.time() * 1000, Temperature]
+    temperature = readpot(0)
+    weight = readpot(1)
+    data = [time.time() * 1000, temperature, weight]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
-if __name__ == '__main__':
-    app.run(debug=True, host='192.168.0.163')
+def thread_webapp():
+    if __name__ == '__main__':
+        app.run(debug=False, host='192.168.0.163')
 
-try:
-    while True:
 
-        warmte = readpot(0)
-        oogst = readpot(1)
+def thread_main():
+    try:
+        while True:       
+            warmte = readpot(0)
+            oogst = readpot(1)
 
-        if warmte > 750!= True:
-            print("WARNING - ", "Temperatuur is Te warm!", warmte , "Graden")
-            warm_sended = True
-            blink(17)
-            time.sleep(2)
+            if warmte > 750 != True:
+                print("WARNING - ", "Temperatuur is Te warm!", warmte, "Graden")
+                warm_sended = True
+                blink(17)
+                time.sleep(2)
 
-        if 200 < warmte < 750!= True:
-            print("Temperatuur is Goed", warmte , "Graden")
-            time.sleep(2)
+            if 200 < warmte < 750 != True:
+                print("Temperatuur is Goed", warmte, "Graden")
+                time.sleep(2)
 
-        if warmte < 200!= True:
-            print("Warning - ", "Temperatuur is Te koud!", warmte ,"Graden")
-            time.sleep(2)
+            if warmte < 200 != True:
+                print("Warning - ", "Temperatuur is Te koud!", warmte, "Graden")
+                time.sleep(2)
 
-        if oogst > 500 != True:
-            print("Warning - ", "Korf is Vol!")
-            time.sleep(2)
-        if 200 < oogst < 500!= True:
-            print("Korf is Goed")
+            if oogst > 500 != True:
+                print("Warning - ", "Korf is Vol!")
+                time.sleep(2)
+            if 200 < oogst < 500 != True:
+                print("Korf is Goed")
 
-        if oogst < 200!= True:
-            print("WARNING ", " Korf is Leeg")
-            time.sleep(2)
+            if oogst < 200 != True:
+                print("WARNING ", " Korf is Leeg")
+                time.sleep(2)
 
-        if GPIO.input(17):
-            time.sleep(1)
-        elif GPIO.input(17) == False:
-            time.sleep(1)
+            if GPIO.input(17):
+                time.sleep(1)
+            elif GPIO.input(17) == False:
+                time.sleep(1)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    client_sock.close()
-    server_sock.close()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        client_sock.close()
+        server_sock.close()
+
+t1 = threading.Thread(target=thread_main)
+t2 = threading.Thread(target=thread_webapp)
+t1.start()
+t2.start()
